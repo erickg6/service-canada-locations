daf1e3cf8732ca908655358b4e25bcd243cbfa03
Greg E <erickg6@gmail.com>
2024-02-06 20:54:17 -0500

    Clean up
    Getting rid of the garbage

--- scrapetest/regiontest.py
+++
@@ -1,3 +0,0 @@
-regions = ['BC', 'AB', 'SK', 'MB', 'ON', 'QC', 'NB', 'NS', 'PE', 'NL', 'YT', 'NU', 'NT']
-for region in regions:
-    print('https://www.servicecanada.gc.ca/tbsc-fsco/sc-lst.jsp?prov='+region+'&lang=eng')
